e9b223d29b22023aca24e334f9ac767bf888f88a
Nikolay <nikolay.kovzik@gmail.com>
2022-01-28 01:28:47 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+
+
+    let openings= Array.from(new Map( bracketsConfig).keys());
+    let closings = Array.from(new Map( bracketsConfig).values());
+    let arr = str.split('');
+    let stack = [];
+    let twins = [];
+    openings.forEach((item) => closings.includes(item) ? twins.push(item) : item);
+    let twinsCounter = {};
+    let unicIndicador;
+
+    if(openings.includes(arr[arr.length-1]) && !twins.includes(arr[arr.length-1]))
+        return false;
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(twins.includes(arr[i]) ) { 
+
+            if(twinsCounter[arr[i]])
+                twinsCounter[arr[i]] += 1;
+            else
+                twinsCounter[arr[i]] = 1;
+
+            unicIndicador = false;
+        }
+        else
+            unicIndicador = true;
+
+        if(closings.indexOf(arr[i]) === -1 || (twinsCounter[arr[i]]%2 !== 0 && unicIndicador!==true)) {
+            stack.push(arr[i])
+        }  else if( closings.indexOf(arr[i]) !== openings.indexOf(stack.pop()) ){
+                 return false;
+            }
+        }
+    
+    
+    return true;
 }
